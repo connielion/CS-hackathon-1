0c2a756b7b8baf09320bdb21371fb4d8cebddcb9
Connie <clai011@ucr.edu>
2019-10-17 02:55:40 -0700

    still need to fix input val

--- background.js
+++ background.js
@@ -1,24 +1,35 @@
 
 // Called when the user clicks on the browser action.
 chrome.browserAction.onClicked.addListener(function (tab) {
-    // Send a message to active tab
+    // Send a message to the active tab
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-        let activeTab = tabs[0];
-        chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_boss_button" });
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, { "message": "clicked_browser_action" });
     });
 });
 
+// This block is new!
 chrome.runtime.onMessage.addListener(
     function (request, sender, sendResponse) {
-        if (request.message === "self_destruct") {
+        if (request.message === "open_new_tab") {
+            //chrome.tabs.create({"url": request.url});
             closeTabs();
+            //saveTabs();
         }
     }
-)
+);
+
+const saveTabs = () => {
+    chrome.tabs.query({}, function (tabs) {
+        for (let i = 0; i < tabs.length; i++) {
+            console.log(tabs[i].id);
+        }
+    });
+}
 
 const closeTabs = () => {
     chrome.tabs.query({}, function (tabs) {
-        for (let i = 0; i < tabs.length; i++) {
+        for (var i = 0; i < tabs.length; i++) {
             chrome.tabs.remove(tabs[i].id);
         }
     });
--- index.js
+++ index.js
@@ -1,42 +1,49 @@
-document.addEventListener('DOMContentLoaded', function () {
-  // FORM SUBMIT EVENT LISTENER
+window.addEventListener('load', function () {
+
   // Google API key: 
   const apiKey = "AIzaSyAP9m96VRNSNyam2zrAAhSBV9YIWok9Cl8";
-
-  //
-  //const searchTerm = "the+office"
   const searchInput = document.getElementById('searchInput');
   console.log(searchInput)
-  let searchContent = searchInput.value;
-  console.log(searchContent)
+  //console.log(searchInput)
+  //let searchContent = searchInput.value;
+  console.log(`outside: `, searchInput.value)
+
   const vidContainer = document.getElementById('videoContainer');
   //console.log(searchTerm)
-  const apiCall = async () => {
+  const apiCall = async (e) => {
+    console.log('INSIDE ASYNC: ', searchInput.value)
+    let searchTerm = searchInput.value;
+    console.log('search term: ', searchTerm) // string
+    const api_call = await fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&order=relevance&q=${searchTerm}&type=video&key=${apiKey}`)
+    const resp = await api_call.json(); // array of objets
 
-    const api_call = await fetch(`https://www.googleapis.com/youtube/v3/search?part=id&q=cat+videos&type=video&key=${apiKey}`)
-    const data = await api_call.json();
-    console.log(data)
-    for (let i = 0; i < data.items.length; i++) {
+    for (let i = 0; i < resp.items.length; i++) {
       // for each video, create iframe tag
       const iframeTag = document.createElement('iframe');
-      let vidId = data.items[i].id.videoId;
+      let vidId = resp.items[i].id.videoId;
       //console.log('video ID: ', vidId)
 
       let url = `https://www.youtube.com/embed/${vidId}`;
-
+      // give each iframe tag a src attribute and set it equal to url
       iframeTag.setAttribute('src', url);
+
+      // append each video into videoContainer
       vidContainer.appendChild(iframeTag);
     }
-    console.log('DATA: ', data);
+    console.log('Response: ', resp);
 
 
   }
+  // FORM SUBMIT EVENT LISTENER
   document.getElementById('form').addEventListener('submit', apiCall());
 
-
   // SUBMIT BUTTON
-  //const submitBtn = document.getElementById('submitBtn');
-  //submitBtn.addEventListener('click', apiCall())
+  function getInput() {
+    let term = document.getElementById('searchInput').value;
+    return term;
+  }
+  const submitBtn = document.getElementById('submitBtn');
+  submitBtn.addEventListener('click', getInput())
 
 
 
