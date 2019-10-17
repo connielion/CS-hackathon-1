7cc6ed79fca273d5c84c9226ae2280005e5ed9b2
Connie <clai011@ucr.edu>
2019-10-16 20:57:41 -0700

    need to fix addListener error

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 // runs on current tab
+//console.log(chrome)
 chrome.runtime.onInstalled.addListener(function () {
     chrome.storage.sync.set({ color: '#3aa757' }, function () {
         console.log("The color is green.");
