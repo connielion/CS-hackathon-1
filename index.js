3e496c1966d1b6d49d91f5efbe8d8cee418ee94c
Connie <clai011@ucr.edu>
2019-10-17 01:43:44 -0700

    appended iframe for each video to videoContainer

--- index.js
+++ index.js
@@ -1,27 +1,43 @@
-// FORM SUBMIT EVENT LISTENER
-// Google API key: 
-const apiKey = "AIzaSyAP9m96VRNSNyam2zrAAhSBV9YIWok9Cl8";
+document.addEventListener('DOMContentLoaded', function () {
+  // FORM SUBMIT EVENT LISTENER
+  // Google API key: 
+  const apiKey = "AIzaSyAP9m96VRNSNyam2zrAAhSBV9YIWok9Cl8";
 
-//let searchContent = document.getElementById('searchInput').value;
-//const searchTerm = "the+office"
-const searchTerm = document.getElementById('searchInput').value;
+  //
+  //const searchTerm = "the+office"
+  const searchInput = document.getElementById('searchInput');
+  console.log(searchInput)
+  let searchContent = searchInput.value;
+  console.log(searchContent)
+  const vidContainer = document.getElementById('videoContainer');
+  //console.log(searchTerm)
+  const apiCall = async () => {
 
-const apiCall = async () => {
+    const api_call = await fetch(`https://www.googleapis.com/youtube/v3/search?part=id&q=cat+videos&type=video&key=${apiKey}`)
+    const data = await api_call.json();
+    console.log(data)
+    for (let i = 0; i < data.items.length; i++) {
+      // for each video, create iframe tag
+      const iframeTag = document.createElement('iframe');
+      let vidId = data.items[i].id.videoId;
+      //console.log('video ID: ', vidId)
 
-  const api_call = await fetch(`https://www.googleapis.com/youtube/v3/search?part=id&q=${searchTerm}&type=video&key=${apiKey}`)
-  const data = await api_call.json();
-  for (let i = 0; i < data.items.length; i++) {
-    console.log('video ID: ', data.items[i].id.videoId)
+      let url = `https://www.youtube.com/embed/${vidId}`;
+
+      iframeTag.setAttribute('src', url);
+      vidContainer.appendChild(iframeTag);
+    }
+    console.log('DATA: ', data);
+
+
   }
-  console.log('DATA: ', data);
-  console.log(searchTerm)
-
-}
-document.getElementById('form').addEventListener('submit', apiCall());
+  document.getElementById('form').addEventListener('submit', apiCall());
 
 
-// SUBMIT BUTTON
-//const submitBtn = document.getElementById('submitBtn');
-//submitBtn.addEventListener('click', apiCall())
+  // SUBMIT BUTTON
+  //const submitBtn = document.getElementById('submitBtn');
+  //submitBtn.addEventListener('click', apiCall())
 
 
+
+})
